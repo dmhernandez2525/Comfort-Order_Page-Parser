50100a286bde8e593437934ec77fa412fb1b7fff
Daniel Hernandez <danielmark252525@gmail.com>
2019-11-29 00:45:03 -0500

    Adds nodemon

--- server/server.js
+++ server/server.js
@@ -2,7 +2,8 @@ const express = require("express");
 const mongoose = require("mongoose");
 const bodyParser = require("body-parser");
 const db = require("../config/keys").MONGO_URI;
-
+const expressGraphQL = require("express-graphql");
+const schema = require("./schema/schema");
 const app = express();
 
 if (!db) {
@@ -19,5 +20,17 @@ mongoose
 
 // remember we use bodyParser to parse requests into json
 app.use(bodyParser.json());
+
+
+
+// use the expressGraphQL middleware to connect our GraphQLSchema to Express
+app.use(
+    "/graphql",
+    expressGraphQL({
+        schema,
+        graphiql: true
+    })
+);
+
 
 module.exports = app;
