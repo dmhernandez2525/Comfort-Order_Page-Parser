652ce563ac58b6a37ba326a09a76c0148372e8b0
Daniel Hernandez <danielmark252525@gmail.com>
2019-12-02 17:10:14 -0500

    uri for heroku

--- client/src/index.js
+++ client/src/index.js
@@ -21,7 +21,11 @@ const cache = new InMemoryCache({
 
 const httpLink = createHttpLink({
   // uri: "http://localhost:5000/graphql"
-  uri: "https://comfort-order.herokuapp.com/graphql"
+  uri: "https://comfort-order.herokuapp.com/graphql",
+  headers: {
+    // pass our token into the header of each request
+    authorization: localStorage.getItem("auth-token")
+  }
 });
 
 const errorLink = onError(({ graphQLErrors }) => {
