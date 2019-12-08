1447e279355f378bf0fc21e41bc7d999090d4858
Jonathan <jonathanjohnson023@gmail.com>
2019-12-07 16:30:55 -1000

    Layout done

---
+++ client/src/components/templates/Restaurant.jsx
@@ -0,0 +1,25 @@
+import React from "react";
+
+class Restaurant extends React.Component {
+  constructor(props){
+    super(props);
+
+    this.state = {
+
+    }
+  }
+
+
+  render(){
+
+    return(
+      <div className="restaurant template">
+        
+      </div>
+    )
+
+  }
+}
+
+
+export default Restaurant
