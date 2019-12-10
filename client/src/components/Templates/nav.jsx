aa8174d6db5751d2b4de000b2efd03861667ae55
Jonathan <jonathanjohnson023@gmail.com>
2019-12-09 21:11:36 -1000

    Changes ig

--- client/src/components/Templates/nav.jsx
+++ client/src/components/Templates/nav.jsx
@@ -12,7 +12,9 @@ class NavBar extends React.Component {
         <li><a href="#teleport-option1">Option 1</a></li>
         <li><a href="#teleport-option2">Option 2</a></li>                       
         <li><a href="#teleport-option3">Option 3</a></li>                       
-        <li><a href="#teleport-option4">Option 4</a></li>                       
+        <li><a href="#teleport-option4">Option 4</a></li> 
+        {/* <li><a href="#one">Option 5</a></li>                        */}
+
         {/* <li><a href={`teleport-${this.props.navOption}`}>Option 5</a></li>
         <li><a href={`teleport-${this.props.navOption}`}>Option 6</a></li>
         <li><a href={`teleport-${this.props.navOption}`}>Option 7</a></li> 
