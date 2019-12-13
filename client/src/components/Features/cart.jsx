0e0b03c1baaeeb3d02875222e67e7e038f9c02c9
Jonathan <jonathanjohnson023@gmail.com>
2019-12-12 21:36:59 -1000

    working on cart

---
+++ client/src/components/Features/cart.jsx
@@ -0,0 +1,23 @@
+import React from "react";
+
+class Cart extends React.Component {
+  constructor(props){
+    super(props)
+      this.state = {
+          
+      }
+  }
+
+  render(){
+
+    return(
+    <div id="cart" className="cart" role="navigation">
+      <ul id="top-menu" className="cart-nav cart-right">
+        
+      </ul>                            
+    </div>
+    )
+  }
+}
+
+export default Cart
