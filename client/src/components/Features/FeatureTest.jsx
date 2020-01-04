a0c3a89e2c619664c212deb0b168351c720d145a
Daniel Hernandez <danielmark252525@gmail.com>
2020-01-04 04:06:24 -0500

    Adds Hours

--- client/src/components/Features/FeatureTest.jsx
+++ client/src/components/Features/FeatureTest.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import TestFe from "./Team" // Just change this route
+import TestFe from "./Hours" // Just change this route
 
 class Test extends React.Component {
   constructor(props){
@@ -10,65 +10,13 @@ class Test extends React.Component {
   render(){
      // change data to the test data that you want   
     let data = {
-        member1: {
-            pic:"https://ca.slack-edge.com/TL86V04VD-UL6LL1UJC-e8394a6f6209-48",
-            name: "Tom",
-            about: "Nullam quis ante. Etiam sit amet orci eget eros faucibus tincidunt. Duis leo. Sed fringilla mauris sit amet nibh. Donec sodales sagittis magna. Aenean commodo ligula.",
-            socialLinks: {
-                facebook:{
-                    url:"https://www.facebook.com",
-                    icon:"https://68ef2f69c7787d4078ac-7864ae55ba174c40683f10ab811d9167.ssl.cf1.rackcdn.com/facebook-icon_32x32.png"
-                },
-                instagram:{
-                    url:"https://www.instagram.com",
-                    icon:"https://68ef2f69c7787d4078ac-7864ae55ba174c40683f10ab811d9167.ssl.cf1.rackcdn.com/instagram-icon_24x24.png"
-                },
-                linkedin:{
-                    url:"https://www.linkedin.com",
-                    icon:"https://68ef2f69c7787d4078ac-7864ae55ba174c40683f10ab811d9167.ssl.cf1.rackcdn.com/linkedin-icon_24x24.png"
-                }
-            },
-        
-        },
-        member2: {
-            pic:"https://ca.slack-edge.com/TL86V04VD-UL6LL1UJC-e8394a6f6209-48",
-            name: "Bill",
-            about: "Nullam quis ante. Etiam sit amet orci eget eros faucibus tincidunt. Duis leo. Sed fringilla mauris sit amet nibh. Donec sodales sagittis magna. Aenean commodo ligula.",
-            socialLinks: {
-                facebook:{
-                    url:"https://www.facebook.com",
-                    icon:"https://68ef2f69c7787d4078ac-7864ae55ba174c40683f10ab811d9167.ssl.cf1.rackcdn.com/facebook-icon_32x32.png"
-                },
-                instagram:{
-                    url:"https://www.instagram.com",
-                    icon:"https://68ef2f69c7787d4078ac-7864ae55ba174c40683f10ab811d9167.ssl.cf1.rackcdn.com/instagram-icon_24x24.png"
-                },
-                linkedin:{
-                    url:"https://www.linkedin.com",
-                    icon:"https://68ef2f69c7787d4078ac-7864ae55ba174c40683f10ab811d9167.ssl.cf1.rackcdn.com/linkedin-icon_24x24.png"
-                }
-            },
-        },
-        member3: {
-            pic:"https://ca.slack-edge.com/TL86V04VD-UL6LL1UJC-e8394a6f6209-48",
-            name: "Margret",
-            about: "Nullam quis ante. Etiam sit amet orci eget eros faucibus tincidunt. Duis leo. Sed fringilla mauris sit amet nibh. Donec sodales sagittis magna. Aenean commodo ligula.",
-            socialLinks: {
-                facebook:{
-                    url:"https://www.facebook.com",
-                    icon:"https://68ef2f69c7787d4078ac-7864ae55ba174c40683f10ab811d9167.ssl.cf1.rackcdn.com/facebook-icon_32x32.png"
-                },
-                instagram:{
-                    url:"https://www.instagram.com",
-                    icon:"https://68ef2f69c7787d4078ac-7864ae55ba174c40683f10ab811d9167.ssl.cf1.rackcdn.com/instagram-icon_24x24.png"
-                },
-                linkedin:{
-                    url:"https://www.linkedin.com",
-                    icon:"https://68ef2f69c7787d4078ac-7864ae55ba174c40683f10ab811d9167.ssl.cf1.rackcdn.com/linkedin-icon_24x24.png"
-                }
-            }
-            
-        }
+      Monday:{from:"10am",to:"10pm"},
+      Tuesday:{from:"10am",to:"10pm"},
+      Wednesday:{from:"10am",to:"10pm"},
+      Thursday:{from:"10am",to:"10pm"},
+      Friday:{from:"10am",to:"10pm"},
+      Saturday:{from:"10am",to:"10pm"},
+      Sunday:{from:"10am",to:"10pm"}
     }
 
 
