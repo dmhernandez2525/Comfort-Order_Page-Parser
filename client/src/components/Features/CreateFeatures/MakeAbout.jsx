9560bd57dbd71176ae73aa9e569d27a85a48fd04
Daniel Hernandez <danielmark252525@gmail.com>
2020-01-04 05:59:30 -0500

    Adds MakeAbout

---
+++ client/src/components/Features/CreateFeatures/MakeAbout.jsx
@@ -0,0 +1,77 @@
+import React from "react";
+import "../../css/MakeFeatures/MakeAbout.css"
+
+class MakeAbout extends React.Component  {
+  constructor(props){
+    super(props)
+    this.state = {
+        title:"",
+        text:"",
+        pic:""
+
+    }
+    this.handleFeatureSubmit = this.props.handleFeatureSubmit
+    this.handleSubmit = this.handleSubmit.bind(this)
+    this.update = this.update.bind(this)
+  }
+
+  handleSubmit(){
+    this.handleFeatureSubmit(this.props.feature, {about: this.state})
+  }
+
+  update(field) {
+    return e => {
+      e.persist()
+        this.setState( state => { 
+        return{
+            [field]:e.target.value
+        }
+      })}
+  }
+
+// EXAMPLE INPUT
+//   data:{
+//       about:{
+//           title: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusm incididunt",
+//           text:"Lorem ipsum dolor sit amet, consectetur adipiscing elit. Maecenas pharetra elit a vehicula viverra. Pellentesque scelerisque nisi eu dui vehicula, ut tristique dui accumsan. Nullam posuere bibendum condimentum. Aenean tempor arcu quis velit ultrices molestie. Etiam dignissim ex id arcu elementum consectetur. Orci varius natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Morbi id dictum velit, ut varius nisi. Nullam lobortis eget lectus sed blandit. Phasellus ultrices felis ac sollicitudin mollis. Phasellus consectetur, sapien nec gravida vehicula, enim metus faucibus sapien, quis gravida libero libero id velit. Fusce augue mi, vestibulum sed dapibus non, faucibus sit amet urna. Praesent bibendum felis condimentum sem accumsan, id maximus velit egestas. Proin ut erat ligula. Etiam eu sollicitudin libero.",
+//           pic: "https://ca.slack-edge.com/TL86V04VD-UL6LL1UJC-e8394a6f6209-48"
+//       }
+//   }
+
+  render(){
+    return(
+    <div className="about-div" > 
+        <h1> About Feature </h1>
+
+        <form className="about-submit" onSubmit={this.handleSubmit }>
+            <div className="days">
+
+                    <input className="about-data"
+                        onChange={this.update("title")}
+                        value={this.state.title}
+                        placeholder="title"
+                    />   
+                    <textarea className="about-data"
+                        onChange={this.update("text")}
+                        value={this.state.text}
+                        placeholder="text"
+                    />                    
+                    {/* rfq this needs to use aws upload */}
+                    <input className="about-data"
+                        onChange={this.update("pic")}
+                        value={this.state.pic}
+                        placeholder="pic"
+                    />                    
+
+            </div>
+          <input type="submit"/>
+        </form>
+    </div>
+    )
+  }
+}
+
+export default MakeAbout
+
+
+
