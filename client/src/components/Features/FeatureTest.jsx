161ec4d8f313a854e1163833cdf9fca3b0f7686c
Jonathan <jonathanjohnson023@gmail.com>
2020-01-04 15:15:38 -1000

    In the middle of testing

--- client/src/components/Features/FeatureTest.jsx
+++ client/src/components/Features/FeatureTest.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import TestFe from "./Pricing" // Just change this route
+import TestFe from "./menu" // Just change this route
 
 class Test extends React.Component {
   constructor(props){
@@ -10,44 +10,14 @@ class Test extends React.Component {
   render(){
      // change data to the test data that you want   
       let data = {
-        row1: {
-            name:"Free",
+        category1: {
+            image:"Free",
             price: 0,
-            details: [
-                "Curabitur eu eros et risus rutrum elementum.",
-                "Phasellus a quam lobortis magna viverra tempus.",
-                "Sed dictum nibh porttitor aliquet pulvinar.",
-                "Pellentesque vestibulum erat eu ex laoreet volutpat.",
-            ]
+            description: "This is the tatsy items description"
         },
-        row2: {
-            name:"Plus",
-            price: 9,
-            details: [
-                "Curabitur eu eros et risus rutrum elementum.",
-                "Phasellus a quam lobortis magna viverra tempus.",
-                "Sed dictum nibh porttitor aliquet pulvinar.",
-                "Pellentesque vestibulum erat eu ex laoreet volutpat.",
-                "Nam tempor ex auctor nunc gravida, vel facilisis eros sagittis.",
-                "Nullam quis eros ac eros cursus tempor a quis lorem.",
-            ]
-        },
-        row3: {
-            name:"Pro",
-            price: 20,
-            details: [
-                "Curabitur eu eros et risus rutrum elementum.",
-                "Phasellus a quam lobortis magna viverra tempus.",
-                "Sed dictum nibh porttitor aliquet pulvinar.",
-                "Pellentesque vestibulum erat eu ex laoreet volutpat.",
-                "Nam tempor ex auctor nunc gravida, vel facilisis eros sagittis.",
-                "Nullam quis eros ac eros cursus tempor a quis lorem.",
-                "Proin vel nisi sed mauris tristique tincidunt vulputate sit amet nulla.",
-                "Phasellus vulputate nisi in libero euismod, lacinia mattis ligula cursus.",
-                "Donec porta enim vitae mollis bibendum.",
-                "Fusce in erat id diam porttitor pellentesque et non tortor."
-            ]
-        }
+        category2:{},
+        category3:{},
+        category4:{},
     }
 
     return(
